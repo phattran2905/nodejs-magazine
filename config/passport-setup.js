3c37cf5492e92ee0457799f84aaef1ccb5422858
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-06-18 16:22:12 +0700

    Fix: setup passport for both admin and user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,36 @@
-const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const AdminModel = require("../models/AdministratorModel");
+const AuthorModel = require("../models/AuthorModel");
 const bcrypt = require("bcrypt");
+const { use } = require("passport");
+
+
+const isUser = async function(id) {
+  try {
+    const user = await AuthorModel.findById(id).exec();
+    
+    if(user) {return true;}
+    
+    return false;
+  } catch (error) {
+    return false;
+  }
+}
+
+const isAdmin = async function(id) {
+  try {
+    const admin = await AdminModel.findById(id).exec();
+
+    if (admin) {return true;}
+
+    return false;
+  } catch (error) {
+    return false;
+  }
+}
 
 const passportSetup = function (passport) {
+
   const authenticateAdmin = async (req, email, password, done) => {
     try {
       const admin = await AdminModel.findOne({ email: email });
@@ -23,7 +50,50 @@ const passportSetup = function (passport) {
     }
   };
 
+  const authenticateUser = async (req, email, password, done) => {
+    try {
+      const user = await AuthorModel.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "Incorrect email!" });
+      }
+      if (!(await bcrypt.compare(password, user.password))) {
+        return done(null, false, { message: "Incorrect password!" });
+      }
+      if (user.status === 'Deactivated'){
+        return done(null, false, { message: "Your account was not allowed to log in!"});
+      }
+
+      return done(null,  user);
+    } catch (error) {
+      return done(error);
+    }
+  };
+
+  passport.serializeUser(async function (obj, done) {
+    if (await isUser(obj.id)){
+      return done(null, {id: obj.id, model: 'user'});
+    } else if (await isAdmin(obj.id)){
+      return done(null, {id: obj.id, model: 'admin'});
+    }
+    // return done(null, obj.id);
+  });
+
+  passport.deserializeUser(async function ({id, model}, done) {
+    if (model === 'user') {
+      AuthorModel.findById(id, function (err, user) {
+        done(err, {loggedUser: user});
+      });
+    } else if(model === 'admin'){
+      AdminModel.findById(id, function (err, admin) {
+        done(err, {loggedAdmin: admin});
+      });
+    } else {
+      done(null,false);
+    }
+  });
+  
   passport.use(
+    'auth-admin',
     new LocalStrategy(
       {
         usernameField: "email",
@@ -32,16 +102,17 @@ const passportSetup = function (passport) {
       authenticateAdmin
     )
   );
-
-  passport.serializeUser(function (admin, done) {
-    done(null, admin.id);
-  });
-
-  passport.deserializeUser(function (id, done) {
-    AdminModel.findById(id, function (err, admin) {
-      done(err, admin);
-    });
-  });
+  
+  passport.use(
+    'auth-user',
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passReqToCallback: true 
+      },
+      authenticateUser
+    )
+  );
 };
 
 module.exports = passportSetup;
