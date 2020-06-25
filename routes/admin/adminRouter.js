688ce5fdb4b01bdbf081536802f77830611fad87
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-06-26 01:32:25 +0700

    Fix: Use checkAuthenticatedAdmin as a middleware

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -9,26 +9,8 @@ const indexRouter = require('./index');
 const authRouter = require('./auth');
 const administratorRouter = require('./administrator');
 
-// app.use(session({
-//     name: 'admin_sid',
-//     secret: "secret_key for @admin",
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         name: 'admin_sessionID',
-//         path: '/admin/',
-//         maxAge: 24*3600*1000*7,
-//         sameSite: true,
-//     }
-// }));
-// adminRouter.use(adminPassport.initialize());
-// adminRouter.use(adminPassport.session());
-// adminPassportSetup(passport);
 authRouter(adminRouter);
-// authRouter(adminRouter, {passport: adminPassport});
-// adminRouter.use(authUtils.checkAuthenticatedAdmin);
-// indexRouter(adminRouter, {passport: adminPassport});
-// administratorRouter(adminRouter, {passport: adminPassport});
+adminRouter.use(authUtils.checkAuthenticatedAdmin);
 indexRouter(adminRouter);
 administratorRouter(adminRouter);
 
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,17 +1,10 @@
-// const express = require("express");
-// const adminRouter = express.Router();
 const authUtils = require("../../utils/authUtils");
 const AdminModel = require("../../models/AdministratorModel");
 
 module.exports = function(adminRouter) {
     
   adminRouter.get("/",
-  authUtils.checkAuthenticatedAdmin,
   async (req, res) => {
-    // console.log(req.session);
-    // console.log('admin: ');
-    // console.log(req.user);
-    // return res.send('logged');
     if (typeof req.session.admin !== 'undefined' && req.session.admin ) {
       const loggedAdmin = {
         username: req.session.admin.username,
@@ -19,7 +12,7 @@ module.exports = function(adminRouter) {
         role: req.session.admin.role,
         status: req.session.admin.status
       }
-      console.log(loggedAdmin);
+      
       if(loggedAdmin) return res.render("admin/index", {loggedAdmin: loggedAdmin});
       // return res.redirect('/login');
     }
