4143e677ba5420d3dc8eda42b3f67449f9e4aa97
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-06-29 15:22:37 +0700

    Fix: Use fn getAuthorPfile instead of getLoggedAuthor

--- routes/user/indexRouter.js
+++ routes/user/indexRouter.js
@@ -1,21 +1,15 @@
-// const express = require('express');
-// const userRouter = express.Router();
+const authUtils = require('../../utils/authUtils');
 
 module.exports = function(userRouter) {
 
     userRouter.get('/',
       async (req,res) => { 
-        let loggedUser;
-        if (typeof req.session.user !== 'undefined' && req.session.user ) {
-          loggedUser = {
-            username: req.session.user.username,
-            email: req.session.user.email,
-            role: req.session.user.role,
-            status: req.session.user.status
-          }
-        }
+        let information = authUtils.getAuthorProfile(req);
         
-        return res.render('user/index', {loggedUser: loggedUser});
+        return res.render('user/index', 
+        {
+          information: information
+        });
     });
 
 };
