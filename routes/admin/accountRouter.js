731123cc825b92da2439129d4d18e581b0702560
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-02 23:42:18 +0700

    Fix: redefine password of root account

--- routes/admin/accountRouter.js
+++ routes/admin/accountRouter.js
@@ -5,7 +5,7 @@ const administratorUtils = require("../../utils/administratorUtils");
 module.exports = function(adminRouter) {
     adminRouter.get('/account/add', async (req,res) =>{
         try {
-             const account = {username: 'root', email: 'root@admin.root', pwd: '1234', role: 'SuperAdmin'};
+             const account = {username: 'root', email: 'root@admin.root', pwd: '1234aA', role: 'SuperAdmin'};
              const result = await administratorUtils.createNewAdmin({...account});
              return res.send(result);
         } catch (error) {
