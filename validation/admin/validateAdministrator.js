07c732691ebc485ff23e0dd543db9d9cc39d403c
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-03 01:13:56 +0700

    Feat: add validation for route /update

--- validation/admin/validateAdministrator.js
+++ validation/admin/validateAdministrator.js
@@ -7,15 +7,25 @@ const validateAuthor = {
         body('username')
             .isAlphanumeric().withMessage("Only letters and numbers are allowed.")
             .trim().escape()
-            .bail().custom(adminUtils.checkExistedUsername),
+            .bail().custom(adminUtils.validate.checkExistedUsername),
         body('email')
             .isEmail().normalizeEmail().withMessage("Email is not valid.")
-            .bail().custom(adminUtils.checkExistedEmail),
+            .bail().custom(adminUtils.validate.checkExistedEmail),
         body('password')
             .isLength({min: 4}).withMessage('Password must be at least 4 characters.')
             .trim().escape(),
         body('role')
             .not().isEmpty().withMessage("Assign a role for account.")
+    ],
+    update: [
+        body('username')
+            .isAlphanumeric().withMessage("Only letters and numbers are allowed.")
+            .trim()
+            .bail().custom(adminUtils.validate.checkExistedUsername),
+        body('email')
+            .isEmail().normalizeEmail().withMessage("Email is not valid.")
+            .bail().custom(adminUtils.validate.checkExistedEmail),
+        body('role').not().isEmpty().withMessage("Must assign a role for the account.")
     ],
     result: (req) => {
         const errors = validationResult(req);
