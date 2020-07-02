f85a56bea6e9db8cccfdb7b692a743bc496bde0e
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-02 21:00:35 +0700

    Feat: update profile for admin

--- routes/admin/profileRouter.js
+++ routes/admin/profileRouter.js
@@ -1,5 +1,7 @@
-const AdminModel = require("../../models/AdministratorModel");
+const adminUtils = require('../../utils/administratorUtils');
 const authUtils = require("../../utils/authUtils");
+const validation = require('../../validation/admin/validateProfile');
+const { update } = require('../../models/AuthorModel');
 
 module.exports = function(adminRouter) {
     adminRouter.get(
@@ -7,10 +9,7 @@ module.exports = function(adminRouter) {
       async (req,res) => {
           res.render('admin/profile/profile_base',
           {
-              page: {
-                  content: 'profile',
-                  content_header: 'profile'
-              },
+              content: 'profile',
               information: authUtils.getAdminProfile(req)
           });
       }
@@ -18,8 +17,42 @@ module.exports = function(adminRouter) {
 
     adminRouter.post(
       '/profile',
+      validation.profile,
       async (req,res) => {
+        const { hasError, errors, validInput } = validation.result(req);
+            
+        if(hasError) { console.log(errors); console.log(validInput);
+            return  res.render('admin/profile/profile_base',{
+                errors: errors, 
+                validInput: validInput,
+                content: 'profile',
+                information: authUtils.getAdminProfile(req)
+            });
+        };
 
+        try { 
+          const updatedQuery = await adminUtils.updateProfile({
+              fullname: req.body.fullname,
+              username: req.body.username,
+              gender: req.body.gender,
+              dob: req.body.dob,
+              phone: req.body.phone
+          });
+
+          if (updatedQuery && updatedQuery.n === 1 && updatedQuery.ok === 1){
+              const reloadedInfo = await authUtils.reloadLoggedAdmin(req, req.session.admin._id);
+              if(reloadedInfo) {
+                  req.flash('success', 'Successfully! Your changes were saved.');
+                  return res.redirect('/admin/profile');
+              }
+          }
+          req.flash('fail', 'Failed! An error occurred during the process.');
+          return res.redirect('/admin/profile');
+
+      } catch (error) {
+          req.flash('fail', 'Failed! An error occurred during the process.');
+          return res.redirect('/admin/profile');
+      }
       }
     );
 }
---
+++ validation/admin/validateProfile.js
@@ -0,0 +1,41 @@
+const {body, validationResult, matchedData} = require('express-validator');
+const authorUtils = require('../../utils/authorUtils');
+const validationUtils = require('../../utils/validationUtils');
+
+const validateProfile = {
+    profile: [
+        body('fullname')
+            .trim()
+            .custom(authorUtils.validate.checkFullnameValid).withMessage('Your name must be alphabetic characters.'),
+        body('dob') // isDate() was removed from express-validator
+            .isBefore('1/1/2004').withMessage('Invalid Date'),
+        body('phone')
+            .isMobilePhone().withMessage('Phone number only contains numeric character.')
+    ],
+    change_password: [
+        body('present_password')
+            .custom(authorUtils.validate.checkPresentPwd).withMessage('Your present password was not correct.'),
+        body('new_password')
+            .isLength({min: 4}).withMessage('Password must be in range of 4-30 characters.')
+            .bail().matches('\[0-9\]').withMessage('Password must have at least a number.')
+            .bail().matches('\[a-z\]').withMessage('Password must have at least a letter.')
+            .bail().matches('\[A-Z\]').withMessage('Password must have at least a capitalized letter.'),
+        body('confirm_password')
+            .custom(validationUtils.checkMatchedNewPassword).withMessage('Confirm new password and New Password must be matched.')
+    ],
+    result: (req) => {
+        const errors = validationResult(req);
+        const validInput = matchedData(req);
+        let hasError = false;
+
+        if(!errors.isEmpty()) hasError = true;
+
+        return {
+            hasError: hasError,
+            errors: errors.array(),
+            validInput: validInput
+        };
+    },
+}
+
+module.exports = validateProfile;
