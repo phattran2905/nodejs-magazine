27c51b06342a44160e7867e827e9c81971391648
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-02 23:37:18 +0700

    Fix: evoke fn checkFullnameValid from validationUtils module

--- validation/admin/validateProfile.js
+++ validation/admin/validateProfile.js
@@ -1,12 +1,13 @@
 const {body, validationResult, matchedData} = require('express-validator');
-const authorUtils = require('../../utils/authorUtils');
+const adminUtils = require('../../utils/administratorUtils');
 const validationUtils = require('../../utils/validationUtils');
 
 const validateProfile = {
     profile: [
         body('fullname')
             .trim()
-            .custom(authorUtils.validate.checkFullnameValid).withMessage('Your name must be alphabetic characters.'),
+            .isLength({min: 6}).withMessage('Fullname must be in range of 4-30 characters.')
+            .custom(validationUtils.checkFullnameValid).withMessage('Your name must be alphabetic characters.'),
         body('dob') // isDate() was removed from express-validator
             .isBefore('1/1/2004').withMessage('Invalid Date'),
         body('phone')
@@ -14,13 +15,14 @@ const validateProfile = {
     ],
     change_password: [
         body('present_password')
-            .custom(authorUtils.validate.checkPresentPwd).withMessage('Your present password was not correct.'),
+            .custom(adminUtils.validate.checkPresentPwd).withMessage('Your present password was not correct.'),
         body('new_password')
             .isLength({min: 4}).withMessage('Password must be in range of 4-30 characters.')
             .bail().matches('\[0-9\]').withMessage('Password must have at least a number.')
             .bail().matches('\[a-z\]').withMessage('Password must have at least a letter.')
             .bail().matches('\[A-Z\]').withMessage('Password must have at least a capitalized letter.'),
         body('confirm_password')
+            .not().isEmpty().withMessage('Confirm new password and New Password must be matched.')
             .custom(validationUtils.checkMatchedNewPassword).withMessage('Confirm new password and New Password must be matched.')
     ],
     result: (req) => {
