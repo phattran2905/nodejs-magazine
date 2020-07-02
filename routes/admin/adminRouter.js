9ad85222292acb99b1778fff84892ec7262ac482
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-02 20:57:11 +0700

    Fix: rename imported modules

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const adminRouter = express.Router();
-const session = require('express-session');
-const passport = require('../../server').passport;
-const adminPassport = require('../../passports/adminPassport');
 const authUtils = require('../../utils/authUtils');
 // Routers
-const indexRouter = require('./index');
-const authRouter = require('./auth');
-const administratorRouter = require('./administrator');
+const indexRouter = require('./indexRouter');
+const authRouter = require('./authRouter');
+const profileRouter = require('./profileRouter');
+// const accountRouter = require('./account');
+const categoryRouter = require('./categoryRouter');
+const articleRouter = require('./articleRouter');
+const authorRouter = require('./authorRouter');
+const administratorRouter = require('./administratorRouter');
 
 authRouter(adminRouter);
 adminRouter.use(authUtils.checkAuthenticatedAdmin);
 indexRouter(adminRouter);
+profileRouter(adminRouter);
+// accountRouter(adminRouter);
+categoryRouter(adminRouter);
+articleRouter(adminRouter);
+authorRouter(adminRouter);
 administratorRouter(adminRouter);
 
 module.exports = adminRouter;
