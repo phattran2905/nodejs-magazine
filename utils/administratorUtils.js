05f9ad97014654eec41cea27a32f68bd39eae4dd
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-04 16:19:49 +0700

    Fix: rewrite validation functions

--- utils/administratorUtils.js
+++ utils/administratorUtils.js
@@ -3,149 +3,183 @@ const commonUtils = require('./commonUtils');
 const bcrypt = require('bcrypt');
 
 const AdminUtils = {
-  // validation
-  validate: {
-    checkExistedEmail: async function (email, {
-      req
-    }) {
-      const match = await AdminModel.findOne({
-        email: email
-      });
-      if (match) {
-        if (typeof req.params.username !== 'undefined' && match.username === req.params.username) {
-          return true;
-        }
-        throw new Error('Email already existed.');
-      }
-      return true;
+    // Express-validator requires returning a Promise
+    // If invalid, return { Promise.reject() }
+    // If valid , return { Promise.resolve() }
+    validate: {
+        checkExistedEmail: async (
+            email = null, 
+            {req} = {}) => {
+            if(!email) {return Promise.reject(false);}
+            
+            const usernameFromReq = (req.params.username) 
+                ? req.params.username 
+                : null;
+            
+            try {
+                const admin = await AdminModel.findOne({
+                    email: email
+                });
+
+                if (admin) {
+                    if (usernameFromReq && admin.username === usernameFromReq) {
+                        return Promise.reject(false);
+                    }
+                    return Promise.resolve(true);
+                }
+                return Promise.reject(false);
+            } catch (error) {
+                return Promise.reject(false);
+            }
+        },
+        checkExistedUsername: async (
+            username = null,
+             {req} = {}) => {
+            if (!username) {return Promise.reject(false)};
+
+            const usernameFromReq = (req.params.username) 
+                ? req.params.username
+                : null;
+            
+            try {
+                const admin = await AdminModel.findOne({
+                    username: username
+                });
+    
+                if (admin) {
+                    if (usernameFromReq && admin.username === usernameFromReq) {
+                        return Promise.reject(false);
+                    }
+                    return Promise.resolve(true);
+                }
+                return Promise.reject(false);
+            } catch (error) {
+                return Promise.reject(false);
+            }
+        },
+        checkPresentPwd: async (pwd = null, {
+            req
+        } = {}) => {
+            if (!pwd || typeof req.session.admin === 'undefined') {
+                return Promise.reject(false)
+            };
+
+            const adminPwd = req.session.admin.password;
+            try {
+                const match = await bcrypt.compare(pwd, adminPwd);
+                if (match) {
+                    return Promise.resolve(true);
+                }
+
+                return Promise.reject(false);
+            } catch (error) {
+                return Promise.reject(false);
+            }
+        },
     },
-    checkExistedUsername: async function (username, {
-      req
-    }) {
-      const match = await AdminModel.findOne({
-        username: username
-      });
-      if (match) {
-        if (typeof req.params.username !== 'undefined' && match.username === req.params.username) {
-          return true;
-        }
-        throw new Error('Username already existed.');
-      }
-      return true;
-    },
-    checkPresentPwd: async (pwd = null, {req} = {}) => {
-        if(!pwd || typeof req.session.admin === 'undefined') {return Promise.reject(false)};
-        
-        const adminPwd = req.session.admin.password;
+    // 
+    createNewAdmin: async ({username, email, pwd, role} = {}) => {
+        if (!username || !email || !pwd || !role) {return null;}
+
         try {
-          console.log(adminPwd);
-            const match = await bcrypt.compare(pwd, adminPwd);
-            if (match) {return Promise.resolve(true);}
-
-            return Promise.reject(false);
+            const hashed_pwd = await bcrypt.hash(pwd, await bcrypt.genSalt(12));
+            const verifyToken = await commonUtils.generateToken(username + email, 7); // valid in 7 days
+            const adminAccount = await AdminModel.create({
+                username: username,
+                email: email,
+                password: hashed_pwd,
+                profile: {
+                    fullname: "",
+                    gender: "Male",
+                    dateOfBirth: "",
+                    phone: ""
+                },
+                verifyToken: {
+                    token: verifyToken.tokenStr,
+                    expiredOn: verifyToken.expiredOn
+                },
+                role: role,
+                status: 'Activated'
+            });
+            
+            return adminAccount;
         } catch (error) {
-            return Promise.reject(false);
+            return null;
         }
     },
-  },
-  // 
-  createNewAdmin: async (adminObj = {
-    username,
-    email,
-    pwd,
-    role
-  }) => {
-    try {
-      adminObj.hashed_pwd = await bcrypt.hash(adminObj.pwd, await bcrypt.genSalt(12));
-      adminObj.verifyToken = await commonUtils.generateToken(adminObj.username + adminObj.email, 7); // valid in 7 days
-      const adminAccount = await AdminModel.create({
-        username: adminObj.username,
-        email: adminObj.email,
-        password: adminObj.hashed_pwd,
-        profile: {
-          fullname: "",
-          gender: "Male",
-          dateOfBirth: "",
-          phone: ""
-        },
-        verifyToken: {
-          token: adminObj.verifyToken.tokenStr,
-          expiredOn: adminObj.verifyToken.expiredOn
-        },
-        role: adminObj.role,
-        status: 'Activated'
-      });
 
-      return adminAccount;
-    } catch (error) {
-      return null;
-    }
-  },
+    updateProfile: async (
+        { fullname, username, gender, dob, phone } = {}
+        ) => {
 
-  updateProfile: async ({fullname, username, gender, dob, phone}) => {
-    if (!fullname || !username || !gender || !dob || !phone) {return null;}
+        if (!fullname || !username || !gender || !dob || !phone) {
+            return null;
+        }
 
-    try {
-        const updateResponse = await AdminModel.updateOne(
-          {username: username},
-            {
+        try {
+            const updateResponse = await AdminModel.updateOne({
+                username: username
+            }, {
                 'profile.fullname': fullname,
                 'profile.gender': gender,
                 'profile.dateOfBirth': new Date(dob), // Parse it to UTC/GMT instead of locale time
                 'profile.phone': phone
-            }
-        );
-        
-        return updateResponse;    
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
-  },
+            });
 
-  update_avatar: async(admin_id = null, {path, contentType, filename, size} = {}) => {
-    if (!admin_id || !path || !contentType || !filename || !size) 
-        {return null;}
-
-    try {
-        const admin = await AdminModel.findById(admin_id);
-        
-        if (admin) {
-            const updateQuery = await AdminModel.updateOne(
-                {_id: admin.id},
-                {
-                    'profile.avatar_img': {
-                        path: path,
-                        contentType: contentType,
-                        filename: filename,
-                        size: size
-                    }
-                }
-            );
-            return updateQuery;
+            return updateResponse;
+        } catch (error) {
+            return null;
         }
-    } catch (error) {
-        return null;
-    }
-    return null;
-  },
-  changePwd: async function({id, new_password}) {
-    if (!id || !new_password) return null;
-    console.log({id, new_password});
-    try {
-        const hashed_pwd = await bcrypt.hash(new_password,await bcrypt.genSalt(12));
-        const updateResponse = await AdminModel.updateOne(
-          {_id: id},
-          {
-            password: hashed_pwd
-          });
+    },
+
+    update_avatar: async (
+        admin_id = null, 
+        { path, contentType, filename, size } = {}
+        ) => {
+        if (!admin_id || !path || !contentType || !filename || !size) {
+            return null;
+        }
+
+        try {
+            const admin = await AdminModel.findById(admin_id);
+
+            if (admin) {
+                const updateQuery = await AdminModel.updateOne(
+                    { _id: admin.id }, 
+                    {
+                        'profile.avatar_img': {
+                            path: path,
+                            contentType: contentType,
+                            filename: filename,
+                            size: size
+                    }
+                    });
+
+                return updateQuery;
+            }
+            return null;
+        } catch (error) {
+            return null;
+        }
+    },
+    changePwd: async  (
+        { id, new_password } = {}
+        ) => {
+        if (!id || !new_password) return null;
         
-        return updateResponse;    
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
-},
+        try {
+            const hashed_pwd = await bcrypt.hash(new_password, await bcrypt.genSalt(12));
+            const updateResponse = await AdminModel.updateOne(
+                { _id: id }, 
+                {
+                    password: hashed_pwd 
+                });
+
+            return updateResponse;
+        } catch (error) {
+            return null;
+        }
+    },
 };
 
 module.exports = AdminUtils;
