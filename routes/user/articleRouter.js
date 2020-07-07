59b932bf1d16b677a087deacc6881166b155d343
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-08 01:22:16 +0700

    Feat: add feature [Update] for articleRouter

--- routes/user/articleRouter.js
+++ routes/user/articleRouter.js
@@ -2,7 +2,7 @@ const CategoryModel = require('../../models/CategoryModel');
 const ArticleModel = require('../../models/ArticleModel');
 const authUtils = require('../../utils/authUtils');
 const articleUtils = require('../../utils/articleUtils');
-const validateArticle = require('../../validation/user/validateArticle');
+const validation = require('../../validation/user/validateArticle');
 const upload = require('../../config/upload-setup');
 
 module.exports = function(userRouter) {
@@ -78,9 +78,9 @@ module.exports = function(userRouter) {
             { name: 'thumbnail_img', maxCount: 1 },
             { name: 'files', maxCount: 3 }
           ]),
-        validateArticle.add,
+          validation.add,
         async (req, res) => {
-            const { hasError, errors, validInput } = validateArticle.result(req);
+            const { hasError, errors, validInput } = validation.result(req);
             
             if(hasError) {
                 const categories = await CategoryModel.find();
@@ -120,7 +120,114 @@ module.exports = function(userRouter) {
         }
     );
 
-    
+    userRouter.get(
+        "/articles/update/:article_id",
+        async (req, res) => {
+          try {
+            const categories = await CategoryModel.find();
+            const article = await articleUtils.getArticleById(req.params.article_id);
+            if(article) {
+              return res.render(
+                  "user/article/article_base", 
+                    { 
+                        content: 'articles',
+                        content: 'article_update',
+                        header: 'Article',
+                        article: article,
+                        categories: categories,
+                        information: authUtils.getAuthorProfile(req)
+                    });
+            }
+  
+            return res.render(
+                "pages/404", 
+                {redirectLink: '/articles'}
+              );
+          } catch (error) {
+              return res.render(
+                  "pages/404", 
+                  {redirectLink: '/articles'}
+                );
+          }
+        }
+      );
+
+      userRouter.post(
+        "/articles/update/:article_id", 
+        upload.fields([
+            { name: 'thumbnail_img', maxCount: 1 },
+            { name: 'files', maxCount: 3 }
+          ]),
+        validation.add,
+        async (req, res) => {
+            try {
+                const categories = await CategoryModel.find();
+                const article = await articleUtils.getArticleById(req.params.article_id);
+                
+                if (article){
+                    const { hasError, errors, validInput } = validation.result(req);
+            
+                    if(hasError) { 
+                        return  res.render(
+                            "user/article/article_base", 
+                            { 
+                                errors: errors, 
+                                validInput: validInput,
+                                content: 'article_update',
+                                header: 'Article',
+                                article: article,
+                                categories: categories,
+                                information: authUtils.getAuthorProfile(req)
+                            });
+                    };
+                    const old_thumbnail = article.thumbnail_img;
+                    const thumbnail = (req.files.thumbnail_img)
+                        ? req.files.thumbnail_img[0]
+                        : {
+                            path: article.thumbnail_img.path,
+                            mimetype: article.thumbnail_img.contentType,
+                            filename: article.thumbnail_img.filename,
+                            size: article.thumbnail_img.size
+                          };
+                    const updateQuery = await articleUtils.updateArticle(
+                        req.params.article_id,
+                        {
+                            categoryId: req.body.categoryId,
+                            title: req.body.title,
+                            summary: req.body.summary,
+                            thumbnail_img: thumbnail, 
+                            body: req.body.body
+                        });
+                    
+                    if (updateQuery && updateQuery.ok === 1 && updateQuery.nModified === 1){
+                        // Remove old thumbnail
+                        if (req.files.thumbnail_img) {
+                            const fs = require('fs');
+                            const path = require('path');
+                            fs.unlinkSync(path.join(process.cwd(),old_thumbnail.path));
+                        }
+
+                        req.flash('success', 'Successfully. All changes were saved.');
+                        return res.redirect("/articles");
+                    }
+
+                    req.flash('fail', 'Failed. An error occurred during the process.');
+                    return res.redirect("/articles");
+                }
+                
+                return res.render(
+                    "pages/404", 
+                    {redirectLink: '/articles'}
+                );
+            } catch (error) {
+                console.log(error)
+                return res.render(
+                    "pages/404", 
+                    {redirectLink: '/articles'}
+                );
+            }
+    });
+
     userRouter.post(
         "/articles/publish", 
         async (req, res) => {
