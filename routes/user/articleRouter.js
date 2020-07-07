3bb3066c5474393a7e35c69ff5f79445a0c11a52
phatductran@wdpc <phat.tranducvn@gmail.com>
2020-07-07 15:50:02 +0700

    Fix: add route for [Create, Preview] for article

--- routes/user/articleRouter.js
+++ routes/user/articleRouter.js
@@ -1,33 +1,122 @@
+const CategoryModel = require('../../models/CategoryModel');
+const ArticleModel = require('../../models/ArticleModel');
 const authUtils = require('../../utils/authUtils');
-const authorUtils = require('../../utils/authorUtils');
-const validateProfile = require('../../validation/user/validateProfile');
+const articleUtils = require('../../utils/articleUtils');
+const validateArticle = require('../../validation/user/validateArticle');
 const upload = require('../../config/upload-setup');
 
 module.exports = function(userRouter) {
     
     userRouter.get(
         '/articles',
-        (req, res) => {
+        async (req, res) => {
+            const categories = await CategoryModel.find();
+            const articles = await ArticleModel.find(
+                {authorId: authUtils.getAuthorProfile(req).id}
+                );
+
+            for(let i = 0; i < categories.length; i++) {
+                for (let j = 0; j < articles.length; j++) {
+                    if (articles[j].categoryId.toString() == categories[i]._id.toString()){
+                        articles[j].categoryName = categories[i].name;
+                    }
+                }
+            }
             res.render('user/article/article_base', {
-                page:  {
-                    content: 'article_list',
-                    header: 'Article'
-                },
+                content: 'article_list',
+                header: 'Article',
+                articles: articles,
                 information: authUtils.getAuthorProfile(req)
             });
         }
     );
 
     userRouter.get(
+        '/articles/preview/:articleId',
+        async (req,res) => {
+            try {
+                const article = await articleUtils.getArticleById(req.params.articleId);
+                if (article) {
+                    return res.render('user/article/article_base', {
+                        content: 'article_preview',
+                        header: 'Article',
+                        article: article,
+                        information: authUtils.getAuthorProfile(req)
+                    });
+                }
+                return res.render(
+                    "pages/404", 
+                    {redirectLink: '/articles'}
+                  );
+            } catch (error) {
+                return res.render(
+                    "pages/404", 
+                    {redirectLink: '/articles'}
+                  );
+            }
+
+        }
+    );
+
+    userRouter.get(
         '/articles/add',
-        (req, res) => {
+        async (req, res) => {
+            const categories = await CategoryModel.find();
+            
             res.render('user/article/article_base', {
-                page:  {
-                    content: 'article_add',
-                    header: 'Article'
-                },
+                content: 'article_add',
+                header: 'Article',
+                categories: categories,
                 information: authUtils.getAuthorProfile(req)
             });
+        }
+    );
+
+    userRouter.post(
+        '/articles/add',
+        upload.fields([
+            { name: 'thumbnail_img', maxCount: 1 },
+            { name: 'files', maxCount: 3 }
+          ]),
+        validateArticle.add,
+        async (req, res) => {
+            const { hasError, errors, validInput } = validateArticle.result(req);
+            
+            if(hasError) {
+                const categories = await CategoryModel.find();
+                return  res.render('user/article/article_base',{
+                    errors: errors, 
+                    validInput: validInput,
+                    content: 'article_add',
+                    header: 'Article',
+                    categories: categories,
+                    information: authUtils.getAuthorProfile(req)
+                });
+            };
+            try {
+                
+                const article = await articleUtils.createNewArticle(
+                    {
+                        authorId: req.body.authorId,
+                        categoryId: req.body.categoryId,
+                        title: req.body.title,
+                        summary: req.body.summary,
+                        thumbnail_img: req.files.thumbnail_img[0], 
+                        body: req.body.body,
+                        publishCheck: req.body.publishCheck
+                    });
+                
+                if(article) {
+                    req.flash('success', 'Successfully! You created a new article');
+                    return res.redirect('/articles/add');
+                }
+
+                req.flash('fail', 'Failed! An error occurred during the process.');
+                return res.redirect('/articles/add');
+            } catch (error) {
+                req.flash('fail', 'Failed! An error occurred during the process.');
+                return res.redirect('/articles/add');
+            }
         }
     );
 
