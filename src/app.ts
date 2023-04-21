835541ca59b57be6703826bb40b36b0f75eca50d
phat@minim1 <phat.tranducvn@gmail.com>
2023-04-20 23:43:32 -0700

    Fix MongoDB Store errors

--- src/app.ts
+++ src/app.ts
@@ -7,43 +7,54 @@ import cookieParser from 'cookie-parser'
 import flash from 'express-flash'
 import path from 'path'
 // import passportSetup from './config/passport-setup'
+import helmet from 'helmet';
+import morgan from 'morgan';
+import ConnectMongoDbSession from 'connect-mongodb-session';
 
 dotenv.config()
 
-const app = express()
-// const MongoStore = require('connect-mongo')(session)
-
-// // Connect to database
+// Connect to database
 mongoose.connect(process.env.DATABASE_URI || 'mongodb://localhost/nodejs_magazine')
 mongoose.connection.once('open', () => console.log('Successfully connected to database'))
 mongoose.connection.on('error', () => {
   console.error.bind(console, 'connection error:')
 })
 
-// app.set('view engine', 'ejs')
-// app.set('views', path.join(__dirname, 'views'))
-// //  middleware
-// app.use('/static', express.static(path.join(__dirname, 'public/user')))
-// app.use('/avatar', express.static(path.join(__dirname, 'uploaded_files/avatar_img')))
-// app.use(
-//   '/thumbnail',
-//   express.static(path.join(__dirname, 'uploaded_files/thumbnail_img'))
-// )
-// app.use('/admin/static', express.static(path.join(__dirname, 'public/admin/')))
-// app.use(express.urlencoded({ extended: false }))
-// app.use(cookieParser())
-// app.use(
-//   session({
-//     name: 'user.id',
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//     secret: process.env.SECRET_KEY || 'phatductran_secret_key',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 24 * 3600 * 1000 * 7,
-//     },
-//   })
-// )
+const MongoDBStore = ConnectMongoDbSession(session)
+const store = new MongoDBStore({
+  uri: process.env.DATABASE_URI || 'mongodb://localhost/nodejs_magazine',
+  collection: 'mySessions'
+});
+
+
+const app = express()
+app.use(helmet())
+app.use(morgan("dev"))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+//  middleware
+app.use('/static', express.static(path.join(__dirname, 'public/user')))
+app.use('/avatar', express.static(path.join(__dirname, 'uploaded_files/avatar_img')))
+app.use(
+  '/thumbnail',
+  express.static(path.join(__dirname, 'uploaded_files/thumbnail_img'))
+)
+app.use('/admin/static', express.static(path.join(__dirname, 'public/admin/')))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  session({
+    name: 'user.id',
+    store: store,
+    secret: process.env.SECRET_KEY || 'phatductran_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 3600 * 1000 * 7,
+    },
+  })
+)
 // app.use(flash())
 // passportSetup(passport)
 // app.use(passport.initialize())
